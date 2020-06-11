4c6851df7ade5fa1d57bb6e014b6789be18146ed
DARKKNIGHTLK <1980242157@qq.com>
2020-06-11 17:38:09 +0800

    LK

---
+++ src/main/java/com/ruoyi/project/test/TestMain.java
@@ -0,0 +1,13 @@
+package com.ruoyi.project.test;
+
+import java.util.Date;
+
+public class TestMain {
+
+    @SuppressWarnings(value = "deprecated")
+    @Hello("Link")
+    public static void main(String[] args) {
+        Date date = new Date(0,0,0);
+
+    }
+}
---
+++ src/main/java/com/ruoyi/project/test/test1/Test1.java
@@ -0,0 +1,5 @@
+package com.ruoyi.project.test.test1;
+
+public class Test1 {
+
+}
